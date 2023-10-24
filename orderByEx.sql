286b6840adc2bc29b09071430785a375535bd398
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-10-24 13:02:08 -0300

    Create orderByEx.sql

---
+++ orderByEx.sql
@@ -0,0 +1,10 @@
+-- Sorting rows with null values
+-- In SQL, null records are considered to have the lowest value, so when sorting ascending we will see them at the beginning and when sorting descending we will see them at the end.
+
+-- In this exercise, we will not learn any new instructions, you will only be asked to sort a table by a specific column and observe how null values ​​are sorted.
+
+-- Exercise:
+-- Sorts the employees table by the 'salary' column in ascending order.
+
+-- CORRECT ANSWER:
+SELECT * FROM employees ORDER BY salary;
