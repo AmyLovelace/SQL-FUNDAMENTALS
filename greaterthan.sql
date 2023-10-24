8796ea2b5be2a32ff03b65ba239313cd9b2756a7
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-10-23 22:55:39 -0300

    added a greater than SQL exercises

---
+++ greaterthan.sql
@@ -0,0 +1,11 @@
+-- USING THE GREATER THAN OR EQUAL TO OPERATOR
+-- The greater than or equal to operator (>=) is used to select records where the value of a column is greater than or equal to a specific value. For example, we can select all products whose price is greater than or equal to $100 using the following query:
+
+-- SELECT * FROM products WHERE price >= 100;
+-- Exercise Select all records from the products table where the value of the 'price' column is greater than or equal to 50.
+
+-- If you were to display only the products with a price greater than 50, would the Desk Lamp be displayed?
+
+-- CORRECT ANSWER:
+
+SELECT * FROM productos WHERE precio >= 50;
