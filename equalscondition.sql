ea5b1fadd219047b076d4a3f6172b1213ab01f1a
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-10-23 23:28:57 -0300

    equals condition

---
+++ equalscondition.sql
@@ -0,0 +1,11 @@
+-- SELECTING ROWS UNDER A CONDITION OF EQUALITY
+-- To select a specific value, we will use the equality operator (=).
+
+-- Example:
+
+-- SELECT * FROM products WHERE price = 100;
+-- Exercise: Select the username of the user from the users table with id equal to 2.
+
+--CORRECT ANSWER:
+
+SELECT username from users WHERE id = 2;
