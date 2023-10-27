ea9cb07a7440e2e0516958382e421aaaef386050
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-10-27 01:18:09 -0300

    Update subqueries.sql

--- subqueries/subqueries.sql
+++ subqueries/subqueries.sql
@@ -63,3 +63,44 @@ SELECT * FROM employees WHERE salary <= (SELECT AVG(salary) FROM employees)
 -- For this type of problem there is usually more than one solution.
 
 SELECT * FROM employees WHERE salary > (SELECT AVG(salary) FROM employees WHERE department = 'Finance')
+
+
+-- Subqueries and where part 2
+-- Exercise
+-- FIRST LAST NAME SALARY DEPARTMENT
+-- Juan Pérez 3000 Sales
+-- María González 3500 Marketing
+-- Carlos Rodríguez 4000 Technology
+-- Ana Martínez 2800 Human Resources
+-- Luis García 3200 Finance
+-- Carmen López 3100 Administration
+-- José Hernández 2900 Operations
+-- Francisco Martín 3400 Legal
+-- Laura Sánchez 3300 Shopping
+-- Antonio Díaz 3600 Production
+-- Sofia Ruiz 2750 Sales
+-- Jorge Vargas 3900 Technology
+-- Elena Castro 3050 Marketing
+-- Pedro Ortega 3150 Finance
+-- Using the data in the Employees table, select all employees whose salary is greater than the employee with the highest salary in the finance department.
+
+SELECT * FROM employees WHERE salary > (SELECT MAX(salary) FROM employees WHERE department = 'Finance')
+
+-- Subqueries and where part 3
+-- Exercise
+-- There is the following notes table:
+
+-- EMAIL NOTES
+-- Student1@example.com 90
+-- Student1@example.com 50
+-- Student1@example.com 30
+-- Student2@example.com 90
+-- Student2@example.com 20
+-- Student3@example.com 80
+-- Student2@example.com 50
+-- Student3@example.com 30
+-- Student3@example.com 10
+-- Select all records above the grade average.
+
+
+SELECT * FROM notes WHERE notes > (SELECT AVG(notes) FROM notes)
